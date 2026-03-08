1e0dedd674f56a879a13dd32d107bb6827226254
NadirFernanda <fernandadapiedade2018gmail.com>
2026-03-08 08:34:39 +0100

    card reembolso para clientes-corrigido

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FreelancerListingController;
 use App\Http\Controllers\FreelancerProfileController;
 use App\Http\Controllers\PublicProjectsController;
